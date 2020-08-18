5f2aed7fbfd80845f234a8eb44fcbca7019f3a13
Juan Manuel Hernandez Martinez <juan.hernandezm@outlook.com>
2020-08-18 00:55:21 -0700

    Create Mantto Controller

---
+++ application/controllers/Mantto.php
@@ -0,0 +1,37 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Mannto extends CI_Controller{
+
+  public function __construct()
+  {
+    parent::__construct();
+    //Codeigniter : Write Less Do More
+  }
+
+  public function index()
+  {
+
+  }
+
+  public function Create($value='')
+  {
+    // code...
+  }
+
+  public function Print($value='')
+  {
+    // code...
+  }
+
+  public function Details($value='')
+  {
+    // code...
+  }
+
+  public function ListMantto($value='')
+  {
+    // code...
+  }
+
+}
