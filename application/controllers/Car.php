f631626ebcb33b2fbc23aab253218d5a4408905f
jp50000 <44383527+jp50000@users.noreply.github.com>
2020-08-20 13:10:35 -0700

    Formularios
    editi,delect,orden

--- application/controllers/Car.php
+++ application/controllers/Car.php
@@ -6,12 +6,43 @@ class Car extends CI_Controller{
   public function __construct()
   {
     parent::__construct();
-    //Codeigniter : Write Less Do More
+    //Codeigniter : Write Less Do Mor
+      $this->load->model(array('Car_model'));
+      
   }
 
   function index()
   {
-    
+    $datos = array('title' =>"Autos" , );
+    $data = array('Car' =>$this->Car_model->GetCar());
+    $this->load->view('Layouts/head',$datos);
+    $this->load->view('Cars/index',$data);
+    $this->load->view('Layouts/footer');
   }
+  public function create()
+  {
+    $id=$this->input->post('Id');
+    $plate=$this->input->post('Plate');
+    $year=$this->input->post('Year');
+    $make=$this->input->post('Make');
+    $model=$this->input->post('Model');
+    $color=$this->input->post('Color');
+    $note=$this->input->post('Notes');
+    $manttoid=$this->input->post('ManttoId');
 
+
+    $data = array('Id' =>$id ,
+    'Plate'=>$plate,
+    'Year'=>$year,
+    'Make'=>$make,
+    'Model'=>$model,
+    'Color'=>$color,
+    'Notes'=>$note);
+    if ($this->Cars_model->AddCar($data)) {
+      redirect(base_url().'Car');
+    }
+    else {
+      echo "Hubo un error";
+    }
+  }
 }
